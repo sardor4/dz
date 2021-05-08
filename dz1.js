d68b9cfb859d8668dd3b8079286a5f81102900c8
sardor4 <83544074+sardor4@users.noreply.github.com>
2021-05-08 13:00:06 +0300

    Add files via upload

---
+++ dz1.js
@@ -0,0 +1,10 @@
+function info() {
+    var name = prompt('Введите имя');
+    var age = +prompt('Введите свой год рождения');
+    var year = +prompt('Введите нынешний год');
+    console.log('Имя: ' + name + ', год рождения: ' + age + ', нынешний год: ' + year);
+    var totalAge = year - age;
+    var x = name + ", Ваш возраст: " + totalAge ;
+    return x;
+}
+console.log(info());
